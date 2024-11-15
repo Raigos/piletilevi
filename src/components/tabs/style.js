78a1df46245466bb6e3221a89b0ec7ebce21cb49
Raigo Tuulik <raigo.tuulik@gmail.com>
2024-11-15 14:12:33 +0200

    Update tabs css

--- src/components/tabs/style.js
+++ src/components/tabs/style.js
@@ -1,9 +1,14 @@
 export const tabsStyles = {
   tabs: {
     borderBottom: '1px solid #d6d2e1',
+    minHeight: '31px',
     '& .MuiTabs-indicator': {
       height: '4px',
       borderRadius: '10px',
+      bottom: '-1px',
+    },
+    '& .MuiButtonBase-root': {
+      minHeight: '31px',
     },
   },
 }
